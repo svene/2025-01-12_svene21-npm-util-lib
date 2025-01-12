5c454db45c1a9d2710d552d4c91c70fb1923aa55
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-01-12 20:25:12 +0100

    added function sub

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,9 @@
-import { add } from './utils.js';
+import { add, sub } from './utils.js';
 import { test, expect } from 'vitest';
 
 test('add', () => {
   expect(add(1, 2)).toBe(3);
+});
+test('sub', () => {
+  expect(sub(1, 2)).toBe(-1);
 });
